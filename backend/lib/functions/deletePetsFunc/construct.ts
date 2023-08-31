ea7717127c088a8c87014925210c817a44924314
focusotter <mtliendo@focusotter.com>
2023-08-31 11:01:10 -0500

    update reference to path

--- backend/lib/functions/deletePetsFunc/construct.ts
+++ backend/lib/functions/deletePetsFunc/construct.ts
@@ -2,7 +2,7 @@ import { PolicyStatement } from 'aws-cdk-lib/aws-iam'
 import { Runtime } from 'aws-cdk-lib/aws-lambda'
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs'
 import { Construct } from 'constructs'
-import path = require('path')
+import * as path from 'path'
 
 type deletePetsFuncProps = {
 	functionName: string

1fd3107fe99a7ca57aa8dba79ba7be4dc7ee0995
focusotter <mtliendo@focusotter.com>
2023-09-01 13:05:10 -0500

    view pets

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,30 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { API } from 'aws-amplify'
 
+type Pet = {
+	id: string
+	name: string
+	owner: string
+	type: string
+}
 export default function Home() {
+	const [pets, setPets] = useState<Pet[]>([])
 	useEffect(() => {
 		API.get('MyPetsAPI', '/pets', {})
-			.then((res) => console.log(res))
+			.then((res) => {
+				return setPets(res as Pet[])
+			})
 			.catch((err) => console.log(err))
 	}, [])
 
-	return <main></main>
+	return (
+		<main>
+			<h1>Pets</h1>
+			<ul>
+				{pets.map((pet) => (
+					<li key={pet.id}>{pet.name}</li>
+				))}
+			</ul>
+		</main>
+	)
 }

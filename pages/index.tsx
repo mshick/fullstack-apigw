89831caad063d4202ce02a24c2bbc999a14fb14d
focusotter <mtliendo@focusotter.com>
2023-09-01 13:00:47 -0500

    use amplify wrapper

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,17 +2,11 @@ import { useEffect } from 'react'
 import { API } from 'aws-amplify'
 
 export default function Home() {
-	// useEffect(() => {
-	// 	API.get('MyPetsAPI', '/pets')
-	// 		.then((res) => console.log(res))
-	// 		.catch((err) => console.log(err))
-	// }, [])
-
 	useEffect(() => {
-		fetch('https://4hbkyijns8.execute-api.us-east-1.amazonaws.com/prod/pets')
-			.then((res) => res.json())
-			.then((data) => console.log({ data }))
-			.catch((err) => console.log({ err }))
+		API.get('MyPetsAPI', '/pets', {})
+			.then((res) => console.log(res))
+			.catch((err) => console.log(err))
 	}, [])
+
 	return <main></main>
 }
